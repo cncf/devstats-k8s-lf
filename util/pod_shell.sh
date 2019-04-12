1d7abe7ed5184a4517e3f96293cd5256a9e15dc8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-12 12:12:26 +0000

    Fix a typo
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/pod_shell.sh
+++ util/pod_shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-  echo "$0: you need to pass  pod name as an argument"
+  echo "$0: you need to pass a pod name as an argument"
   exit 2
 fi
 kubectl exec -it "$1" -- /bin/bash
