8e028b102f5e498f52ab885dcd28843fc5fc20e9
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-25 06:38:19 +0000

    Analyze cronjob logs done
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/recent_cronjobs_logs.sh
+++ util/recent_cronjobs_logs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-if [ -z "$1" ]
+if ( [ -z "$1" ] || [ -z "$2" ] )
 then
-  echo "$0: you need to provide all_test_projects.txt file"
+  echo "$0: you need to provide all_test_projects.txt output.log arguments"
   exit 1
 fi
+> "$2"
 pods=`LAST=1 ./list_projects_objects.sh $1 po 'devstats-'`
 for pod in $pods
 do
-  echo "pod: $pod"
+  echo "pod '$pod'" >> "$2"
+  kubectl logs $pod >> "$2"
 done
