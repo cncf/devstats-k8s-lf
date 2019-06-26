ab1038508f3a25199656169e67b93938193060d5
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-26 04:20:03 +0000

    Logs for test and prod [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util/logs_prod.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./recent_cronjobs_logs.sh ../../devstats-docker-images/devstats-helm/all_prod_projects.txt logs.txt
---
+++ util/logs_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./recent_cronjobs_logs.sh ../../devstats-docker-images/devstats-helm/all_test_projects.txt logs.txt
