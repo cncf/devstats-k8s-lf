216a88d5c1d84f02288e8070c000d1ee3cddc6aa
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-10 08:00:34 +0000

    Updated reporting tool
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/report_state.sh
+++ util/report_state.sh
@@ -11,6 +11,62 @@ do
   ns=${arr[0]}
   pod=${arr[1]}
   sts=${arr[2]}
-  echo "===== namespace: $ns, pod: $pod =====" >> report.txt
+  echo "===== namespace: $ns, pod: $pod, status: $sts =====" >> report.txt
+  kubectl -n "$ns" logs "$pod" --all-containers | tail -n 100 >> report.txt
+done
+
+echo "========== CRASH LOOP BACK OFF PODS ==========" >> report.txt
+objs=`ALL=1 ./list_pods.sh CrashLoopBackOff`
+for obj in $objs
+do
+  IFS=':'
+  arr=($obj)
+  unset IFS
+  ns=${arr[0]}
+  pod=${arr[1]}
+  sts=${arr[2]}
+  echo "===== namespace: $ns, pod: $pod, status: $sts =====" >> report.txt
+  kubectl -n "$ns" logs "$pod" --all-containers | tail -n 100 >> report.txt
+done
+
+echo "========== ImagePullBackOff PODS ==========" >> report.txt
+objs=`ALL=1 ./list_pods.sh ImagePullBackOff`
+for obj in $objs
+do
+  IFS=':'
+  arr=($obj)
+  unset IFS
+  ns=${arr[0]}
+  pod=${arr[1]}
+  sts=${arr[2]}
+  echo "===== namespace: $ns, pod: $pod, status: $sts =====" >> report.txt
+  kubectl -n "$ns" logs "$pod" --all-containers | tail -n 100 >> report.txt
+done
+
+echo "========== CONTAINER CREATING PODS ==========" >> report.txt
+objs=`ALL=1 ./list_pods.sh ContainerCreating`
+for obj in $objs
+do
+  IFS=':'
+  arr=($obj)
+  unset IFS
+  ns=${arr[0]}
+  pod=${arr[1]}
+  sts=${arr[2]}
+  echo "===== namespace: $ns, pod: $pod, status: $sts =====" >> report.txt
+  kubectl -n "$ns" logs "$pod" --all-containers | tail -n 100 >> report.txt
+done
+
+echo "========== PENDING PODS ==========" >> report.txt
+objs=`ALL=1 ./list_pods.sh Pending`
+for obj in $objs
+do
+  IFS=':'
+  arr=($obj)
+  unset IFS
+  ns=${arr[0]}
+  pod=${arr[1]}
+  sts=${arr[2]}
+  echo "===== namespace: $ns, pod: $pod, status: $sts =====" >> report.txt
   kubectl -n "$ns" logs "$pod" --all-containers | tail -n 100 >> report.txt
 done
