78e6f12c06c5a47cbfb2b6ea1d7cb27b2f0135db
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-11 12:52:16 +0000

    new scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util/cleanup_all_completed_pods.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# DRYRUN=1 (only display what would be done)
+pods=""
+for data in `kubectl get po -o=jsonpath='{range .items[*]}{.metadata.namespace}{";"}{.metadata.name}{";"}{.status.phase}{"\n"}{end}' --all-namespaces`
+do
+  IFS=';'
+  arr=($data)
+  unset IFS
+  ns=${arr[0]}
+  pod=${arr[1]}
+  sts=${arr[2]}
+  if [ "$sts" = "Succeeded" ]
+  then
+    pods="${pods} ${ns}:${pod}"
+  fi
+done
+if [ ! -z "$pods" ]
+then
+  if [ -z "$DRYRUN" ]
+  then
+    echo "Deleting pods: ${pods}"
+    for data in $pods
+    do
+      IFS=':'
+      arr=($data)
+      unset IFS
+      ns=${arr[0]}
+      pod=${arr[1]}
+      kubectl delete po -n "$ns" "$pod"
+    done
+  else
+    echo "Would delete pods: ${pods}"
+  fi
+fi
