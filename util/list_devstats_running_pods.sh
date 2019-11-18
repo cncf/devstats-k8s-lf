2b8af9f2b75f8dd1cbd272a7a8185961ae02ecb5
root <root@master.k8s.devstats.cncf.io>
2019-11-18 12:53:58 +0000

    Update script
    Signed-off-by: root <root@master.k8s.devstats.cncf.io>

--- util/list_devstats_running_pods.sh
+++ util/list_devstats_running_pods.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 kubectl get po --all-namespaces | grep Running | grep -v devstats-grafana | grep 'devstats-test\|devstats-prod' | grep -v 'devstats-postgres\|devstats-static\|nginx'
+kubectl get po --all-namespaces | grep 'Error\|Crash\|Loop\|Creating\|Pending'
