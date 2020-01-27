5cc10ce18b54dcf8d0792b7ec8c017ecda62953b
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-01-27 15:52:15 +0000

    Fix detecting issues [wip]
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/check_status.sh
+++ util/check_status.sh
@@ -2,4 +2,4 @@
 ./list_devstats_running_pods.sh
 ./logs_test.sh
 ./logs_prod.sh
-cat logs_test.txt logs_prod.txt | grep -i -E '([^[:alnum:]_]|^)error([^[:alnum:]_]|$)+'
+cat logs_test.txt logs_prod.txt | grep -i -E '([^[:alnum:]_]|^)errors?([^[:alnum:]_]|$)+'
