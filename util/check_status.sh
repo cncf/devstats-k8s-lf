47e346f7254216d2c09da116ea555563237621b0
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-06 15:49:21 +0000

    Updates
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/check_status.sh
+++ util/check_status.sh
@@ -2,4 +2,9 @@
 ./list_devstats_running_pods.sh
 ./logs_test.sh
 ./logs_prod.sh
-cat logs_test.txt logs_prod.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
+echo 'Test status:'
+cat logs_test.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
+echo '------------'
+cat logs_prod.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
+echo 'Test status:'
+echo '------------'
