29282b90f6a0e1125bc9eae26eb00b45d4624c7b
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-07-06 06:25:02 +0000

    Adopt for cronjob
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/check_freedesktop.sh
+++ util/check_freedesktop.sh
@@ -4,11 +4,11 @@
 # correct status example
 # org.freedesktop.systemd1 1 systemd root :1.4157018 init.scope - -
 #sts=`echo 'org.freedesktop.systemd1               - -               -                (activatable) -                         -' | grep org.freedesktop.systemd1 | awk '{print $6}'`
-sts=`busctl --list | grep org.freedesktop.systemd1 | awk '{print $6}'`
+sts=`/usr/bin/busctl --list | grep org.freedesktop.systemd1 | awk '{print $6}'`
 #echo $sts
 #if [ ! "$sts" = "init.scope" ]
 if [ "$sts" = "-" ]
 then
-  echo "org.freedesktop.systemd1 broken, doing systemctl daemon-reexec"
-  systemctl daemon-reexec
+  echo "org.freedesktop.systemd1 broken, doing /bin/systemctl daemon-reexec"
+  /bin/systemctl daemon-reexec
 fi
