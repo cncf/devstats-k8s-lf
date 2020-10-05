3418ce7011df355ad017cd983d9f0d9687d456e0
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-10-05 16:36:42 +0000

    Fix
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/cleanup_all_evicted_pods.sh
+++ util/cleanup_all_evicted_pods.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for d in `k get po --all-namespaces | grep cert-manager | grep Evicted | awk '{print $1";;;"$2}'`
+for d in `k get po --all-namespaces | grep Evicted | awk '{print $1";;;"$2}'`
 do
   cmd="kubectl delete po -n ${d/;;;/ }"
   echo "$cmd"
-  `$cmd`
+  eval $cmd
 done
