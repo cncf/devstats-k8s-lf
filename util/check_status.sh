43ff72ae7bbd031509c29cbe4a0882bd08a85e37
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-10-30 12:45:02 +0000

    Update script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/check_status.sh
+++ util/check_status.sh
@@ -6,9 +6,11 @@ df -ih | grep '/dev/sda'
 kubectl describe nodes | grep HasDiskPressure
 ./list_devstats_running_pods.sh
 echo 'Test status:'
+k exec -itn devstats-test devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running
 ./logs_test.sh
 cat logs_test.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
 echo 'Prod status:'
+k exec -itn devstats-prod devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running
 ./logs_prod.sh
 cat logs_prod.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
 echo '------------'
