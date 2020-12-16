02959c2f981aa24db47a64803f74b093bc4e0710
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-12-16 11:17:04 +0000

    Update for K8s v1.20 and patroni PostgreSQL 13
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/check_status.sh
+++ util/check_status.sh
@@ -6,11 +6,11 @@ df -ih | grep '/dev/sda'
 kubectl describe nodes | grep HasDiskPressure
 ./list_devstats_running_pods.sh
 echo 'Test status:'
-k exec -itn devstats-test devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running
+kubectl exec -itn devstats-test devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running | grep -v Cluster
 ./logs_test.sh
 cat logs_test.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
 echo 'Prod status:'
-k exec -itn devstats-prod devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running
+kubectl exec -itn devstats-prod devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running | grep -v Cluster
 ./logs_prod.sh
 cat logs_prod.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
 echo '------------'
