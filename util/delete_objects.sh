59b80b341ef062ce527b81cdbf110200748556bd
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-06-09 05:52:38 +0000

    Add iterative delete mode
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/delete_objects.sh
+++ util/delete_objects.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # DRYRUN=1 (only display what would be done)
+# ITER=1 (delete objects one at a time, not all at once)
 if [ -z "$1" ]
 then
   echo "$0: you need to provide object type as an argument, example: po, svc"
@@ -26,12 +27,26 @@ if [ ! -z "$pods" ]
 then
   if [ -z "$DRYRUN" ]
   then
-    echo "Deleting $1: ${pods}"
-    if [ -z "$3" ]
+    if [ -z "$ITER" ]
     then
-      kubectl delete $1 ${pods}
+      echo "Deleting $1: ${pods}"
+      if [ -z "$3" ]
+      then
+        kubectl delete $1 ${pods}
+      else
+        kubectl delete $1 ${pods} "${@:3:99}"
+      fi
     else
-      kubectl delete $1 ${pods} "${@:3:99}"
+      for pod in $pods
+      do
+        echo "Deleting $1: ${pod}"
+        if [ -z "$3" ]
+        then
+          kubectl delete $1 ${pod}
+        else
+          kubectl delete $1 ${pod} "${@:3:99}"
+        fi
+      done
     fi
   else
     echo "Would delete $1: ${pods}"
