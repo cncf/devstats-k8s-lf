e845e58c84f03e753e25ecec35a735e050557699
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2022-01-12 12:57:08 +0000

    Update check status script 2
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/check_status.sh
+++ util/check_status.sh
@@ -16,5 +16,5 @@ cat logs_prod.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)
 echo '------------'
 ./check_calico.sh
 echo 'Recent affiliations imports'
-kubectl get po -A | grep affiliations | grep -E '\s+[0-9]+h$'
+kubectl get po -A | grep affiliations | grep -E '\s+[0-9]+h([0-9]+m)?$'
 echo '------------'
