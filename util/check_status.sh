158e0a6d33ccbde3cf064c806a237208726a0866
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2022-04-22 07:08:22 +0000

    Add reporting if log size > 1G
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/check_status.sh
+++ util/check_status.sh
@@ -7,10 +7,12 @@ kubectl describe nodes | grep HasDiskPressure
 ./list_devstats_running_pods.sh
 echo 'Test status:'
 kubectl exec -itn devstats-test devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running | grep -v Cluster
+kubectl exec -itn devstats-test devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -E '[[:digit:]]{4,}' | grep -v Cluster
 ./logs_test.sh
 cat logs_test.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
 echo 'Prod status:'
 kubectl exec -itn devstats-prod devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running | grep -v Cluster
+kubectl exec -itn devstats-prod devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -E '[[:digit:]]{4,}' | grep -v Cluster
 ./logs_prod.sh
 cat logs_prod.txt | grep -i -E '([^[:alnum:]_]|^)(errors?|flag)([^[:alnum:]_]|$)+'
 echo '------------'
