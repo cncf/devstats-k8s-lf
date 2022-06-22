3907da17fc2b17d6e48c3ae6f14f7cc1278d501c
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2022-06-22 05:46:54 +0000

    Add cron support to run check healths from node-level cron job
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/check_patroni_health.sh
+++ util/check_patroni_health.sh
@@ -4,10 +4,10 @@ then
   echo "$0: you need to specify env: test or prod"
   exit 1
 fi
-# kubectl exec -itn devstats-$1 devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running | grep -v Cluster
-# kubectl exec -itn devstats-$1 devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -E '[[:digit:]]{4,}' | grep -v Cluster
+# kubectl exec -n devstats-$1 devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -v running | grep -v Cluster
+# kubectl exec -n devstats-$1 devstats-postgres-0 -- /usr/local/bin/patronictl list | grep devstats-postgres | grep -E '[[:digit:]]{4,}' | grep -v Cluster
 # Check if we need to reinit lagging replica
-r=$(kubectl exec -itn devstats-${1} devstats-postgres-0 -- patronictl list -f json | jq --compact-output -rS)
+r=$(kubectl exec -n devstats-${1} devstats-postgres-0 -- patronictl list -f json | jq --compact-output -rS)
 s0=$(echo -n "${r}" | jq -r '.[0].State')
 s1=$(echo -n "${r}" | jq -r '.[1].State')
 s2=$(echo -n "${r}" | jq -r '.[2].State')
@@ -16,6 +16,11 @@ s="${s0},${s1},${s2},${s3}"
 reinit=""
 reinits=""
 other=""
+now=$(date)
+if [ ! -z "${VERBOSE}" ]
+then
+  echo "${now}: starting ${1} patroni health"
+fi
 for n in 0 1 2 3
 do
   v="s${n}"
@@ -25,13 +30,13 @@ do
     state=$(echo -n "${r}" | jq -rS ".[${n}].State")
     if ( [ "${state}" = "stopping" ] || [ "${state}" = "creating replica" ] )
     then
-      echo "Node #${n} state: ${state}"
+      echo "${now}: Node #${n} state: ${state}"
       if [ "${state}" = "creating replica" ]
       then
         other=${n}
       fi
     else
-      echo "Node #${n} is not running: ${data}"
+      echo "${now}: Node #${n} is not running: ${data}"
       exit 0
     fi
   fi
@@ -43,10 +48,10 @@ do
   lag=$(echo -n "${r}" | jq -r ".[${n}].\"Lag in MB\"")
   if (( ${lag} > 2048 ))
   then
-    echo "Node #${n} lags more than 2 Gb: ${lag} MB"
+    echo "${now}: Node #${n} lags more than 2 Gb: ${lag} MB"
     if (( ${lag} > 20480 ))
     then
-      echo "Node #${n} lags more than 20 Gb, scheduling reinit if all other nodes healthy"
+      echo "${now}: Node #${n} lags more than 20 Gb, scheduling reinit if all other nodes healthy"
       if [ -z "${reinit}" ]
       then
         reinit=${n}
@@ -64,10 +69,13 @@ if [ ! -z "${reinit}" ]
 then
   if [ "$s" = "running,running,running,running" ]
   then
-    echo "Detected reinit condition for Node #${reinit}, all reinits needed: ${reinits}"
-    echo "kubectl exec -itn devstats-${1} devstats-postgres-0 -- patronictl reinit devstats-postgres devstats-postgres-${reinit} --force"
-    kubectl exec -itn devstats-${1} devstats-postgres-0 -- patronictl reinit devstats-postgres devstats-postgres-${reinit} --force
+    echo "${now}: Detected reinit condition for Node #${reinit}, all reinits needed: ${reinits}"
+    echo "${now}: kubectl exec -itn devstats-${1} devstats-postgres-0 -- patronictl reinit devstats-postgres devstats-postgres-${reinit} --force"
+    kubectl exec -n devstats-${1} devstats-postgres-0 -- patronictl reinit devstats-postgres devstats-postgres-${reinit} --force
   else
-    echo "Nodes ${reinits} need to be reinitialized, but other replica ${other} reinitialize is in progress"
+    echo "${now}: Nodes ${reinits} need to be reinitialized, but other replica ${other} reinitialize is in progress"
   fi
+elif [ ! -z "${VERBOSE}" ]
+then
+  echo "${now}: ${1} patroni health OK"
 fi
