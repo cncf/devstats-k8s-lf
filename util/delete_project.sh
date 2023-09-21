d756f2babf858aee3d5d15cd256d0c804072e750
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2023-09-21 12:45:13 +0000

    Support delete project
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util/delete_project.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# [NS=devstats-test]
+# [OP=delete]
+# [MN=0]
+if [ -z "${1}" ]
+then
+  echo "$0: you need to specify project name as a 1st argument"
+  exit 1
+fi
+if [ -z "${NS}" ]
+then
+  export NS='devstats-test'
+fi
+if [ -z "${OP}" ]
+then
+  export OP='get'
+  echo "$0: to actually delete specify OP=delete"
+fi
+if [ -z "${MN}" ]
+then
+  export MN=0
+fi
+kubectl get all -n "${NS}" | grep "${1}"
+kubectl "${OP}" -n "${NS}" cronjob "devstats-${1}" "devstats-affiliations-${1}"
+kubectl "${OP}" -n "${NS}" deployment "devstats-grafana-${1}"
+kubectl "${OP}" -n "${NS}" service "devstats-service-${1}"
+kubectl exec -in "${NS}" "devstats-postgres-${MN}" -- psql "${1}" -c 'select count(*) from gha_events'
+kubectl exec -in "${NS}" "devstats-postgres-${MN}" -- psql -c "drop database ${1}"
