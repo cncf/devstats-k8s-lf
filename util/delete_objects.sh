3157b319daeaa20d6ef96c32b3efd96aff61589f
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2025-09-23 04:13:44 +0000

    Support REGEXP when deleting objects
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util/delete_objects.sh
+++ util/delete_objects.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # DRYRUN=1 (only display what would be done)
 # ITER=1 (delete objects one at a time, not all at once)
+# RE=1 (treat 2nd argument as regexp instead of prefix)
 if [ -z "$1" ]
 then
   echo "$0: you need to provide object type as an argument, example: po, svc"
@@ -12,15 +13,23 @@ then
   exit 1
 fi
 otype=$1
-olen=${#2}
+pattern=$2
 objs=`kubectl get $1`
 pods=""
 for obj in $objs
 do
-  base=${obj:0:$olen}
-  if [ "$base" = "$2" ]
+  if [ -z "$RE" ]
   then
-    pods="$pods $obj"
+    base=${obj:0:${#pattern}}
+    if [ "$base" = "$pattern" ]
+    then
+      pods="$pods $obj"
+    fi
+  else
+    if [[ "$obj" =~ $pattern ]]
+    then
+      pods="$pods $obj"
+    fi
   fi
 done
 if [ -z "$3" ]
