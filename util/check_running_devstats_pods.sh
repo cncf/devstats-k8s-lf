75379e7b1af8889dbba6306802cbf1036b104825
Łukasz Gryglicki <lgryglicki@cncf.io>
2026-01-04 09:04:31 +0000

    Update script
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- util/check_running_devstats_pods.sh
+++ util/check_running_devstats_pods.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl get po -A | grep -v '\-grafana-' | grep -v '\-api-' | grep -v postgres | grep -v ingress | grep -v static | grep -v reinit | grep -v fix | grep -v provision | grep -E 'devstats-(test|prod)' | grep Running
+kubectl get po -A | grep -v '\-grafana-' | grep -v '\-api-' | grep -v postgres | grep -v ingress | grep -v static | grep -v reinit | grep -v fix | grep -v provision | grep -v debug | grep -v reports | grep -E 'devstats-(test|prod)' | grep Running
